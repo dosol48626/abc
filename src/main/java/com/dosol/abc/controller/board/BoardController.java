941f06fec354b36f000e4f1363083344936a4514
DOSOL\dosol <dosol48626@naver.com>
2024-11-08 10:38:26 +0900

    Board CRUD 작업 완료. 노트에 이미지 업로드 경로 c에 잡아주셔야 오류 안나요.

--- src/main/java/com/dosol/abc/controller/board/BoardController.java
+++ src/main/java/com/dosol/abc/controller/board/BoardController.java
@@ -43,9 +43,44 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
+//    @GetMapping("/read")
+//    public void readGET(Model model, Long boardId,  HttpSession session) {
+//        User user = (User)session.getAttribute("user");
+//
+//        BoardDTO boardDTO = boardService.readOne(boardId);
+//        model.addAttribute("boardDTO", boardDTO);
+//    }
+
     @GetMapping("/read")
-    public void readGET(Model model, Long boardId) {
+    public void readGET(Model model, Long boardId, HttpSession session) {
+        User user = (User)session.getAttribute("user");
+
+        // 세션에 로그인된 사용자의 username을 모델에 추가
+        if (user != null) {
+            model.addAttribute("username", user.getUsername());
+        }
+
+        // 게시글 데이터를 가져와 모델에 추가
         BoardDTO boardDTO = boardService.readOne(boardId);
         model.addAttribute("boardDTO", boardDTO);
+    }
+
+    @GetMapping("/modify")
+    public void modifyGET(Model model, Long boardId) {
+        BoardDTO boardDTO = boardService.readOne(boardId);
+        model.addAttribute("boardDTO", boardDTO);
+    }
+
+    @PostMapping("/modify")
+    public String modifyPOST(BoardDTO boardDTO) {
+
+        boardService.modify(boardDTO);
+        return "redirect:/board/read?boardId=" + boardDTO.getBoardId();
+    }
+
+    @GetMapping("/remove")
+    public String removeGET(Long boardId) {
+        boardService.remove(boardId);
+        return "redirect:/board/list";
     }
 }
