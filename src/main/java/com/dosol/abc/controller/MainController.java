d77e6219044fd1825004aeef0451a40822139a55
DOSOL\dosol <dosol48626@naver.com>
2024-11-13 10:40:09 +0900

    최종

--- src/main/java/com/dosol/abc/controller/MainController.java
+++ src/main/java/com/dosol/abc/controller/MainController.java
@@ -2,45 +2,46 @@ package com.dosol.abc.controller;
 
 import com.dosol.abc.domain.user.User;
 import com.dosol.abc.domain.user.UserImage;
+import com.dosol.abc.domain.wise.Wise;
+import com.dosol.abc.dto.wise.WiseDTO;
 import com.dosol.abc.service.user.UserService;
+import com.dosol.abc.service.wise.WiseService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
+@Log4j2
 public class MainController {
 
     private final UserService userService;
     private final HttpSession session;
     private final ModelMapper modelMapper;
+    private final WiseService wiseService;
 
-//    @GetMapping("/")
-//    public String mainP(HttpSession session, Model model) {
-//
-//        User user = (User) session.getAttribute("user");
-//
-//        if (user != null) {
-//
-//            model.addAttribute("user", user);
-//        }
-//        return "main";
-//    }
 
     @GetMapping("/")
     public String mainPage(HttpSession session, Model model) {
         User user = (User) session.getAttribute("user");
         if (user != null) {
             model.addAttribute("user", user);
-
-            // 프로필 이미지 조회 및 전달
             UserImage profileImage = userService.getProfileImageByUsername(user.getUsername());
             model.addAttribute("profileImage", profileImage);
         }
-        return "main"; // main.html 페이지로 이동
+
+        // 명언 조회
+        WiseDTO wiseDTO = wiseService.getRandomWise();
+        log.info("@@@@@@@@@!!!!!!!!!!!!!!명어어어어ㅓ어어어어어어어ㅓ어어어어어어ㅓㄴ"+wiseDTO);
+
+        model.addAttribute("wise", wiseDTO);
+
+        return "main";
     }
 
     @GetMapping("/on")
@@ -54,4 +55,5 @@ public class MainController {
 
         return "mypage";
     }
+
 }
